a9aa4578fed19f4595594d11b7c249ae63f38cb3
ZoDream <648383079@qq.com>
2020-03-28 22:45:34 +0800

    提取basic token方法

--- src/Service/Controller.php
+++ src/Service/Controller.php
@@ -2,44 +2,26 @@
 namespace Zodream\Module\OAuth\Service;
 
 use Zodream\Infrastructure\Http\Request;
-use Zodream\Service\Config;
 use Zodream\Route\Controller\ModuleController as BaseController;
-use Zodream\Service\Rest\OAuth\Exception\OAuthServerException;
 
 abstract class Controller extends BaseController {
 
     protected function validateClient() {
-        list($basicAuthUser, $basicAuthPassword) = $this->getBasicAuthCredentials();
-        $clientId = app('request')->get('client_id', $basicAuthUser);
+        /** @var Request $request */
+        $request = app('request');
+        list($basicAuthUser, $basicAuthPassword) = $request->basicToken();
+        $clientId = $request->get('client_id', $basicAuthUser);
         if (is_null($clientId)) {
-            throw OAuthServerException::invalidRequest('client_id');
+            throw new \Exception('client_id');
         }
 
         // If the client is confidential require the client secret
-        $clientSecret = app('request')->get('client_secret', $basicAuthPassword);
+        $clientSecret = $request->get('client_secret', $basicAuthPassword);
 
         // If a redirect URI is provided ensure it matches what is pre-registered
-        $redirectUri = app('request')->get('redirect_uri', null);
+        $redirectUri = $request->get('redirect_uri', null);
     }
 
-    protected function getBasicAuthCredentials() {
-        $header = app('request')->header('Authorization');
-        if (empty($header)) {
-            return [null, null];
-        }
-        if (is_array($header)) {
-            $header = current($header);
-        }
-        if (strpos($header, 'Basic ') !== 0) {
-            return [null, null];
-        }
-        if (!($decoded = base64_decode(substr($header, 6)))) {
-            return [null, null];
-        }
-        if (strpos($decoded, ':') === false) {
-            return [null, null]; // HTTP Basic header without colon isn't valid
-        }
-        return explode(':', $decoded, 2);
-    }
+
 
 }
